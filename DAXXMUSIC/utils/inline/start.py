b5497ef948269951863f7396e827b81a5ad05100
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-04-11 01:49:55 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -37,6 +37,10 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_12"],
+                url=f"https://whatsapp.com/channel/0029VaaXW4X4CrfdPGJrFg11",
+            )
         ],   
     ]
     return buttons
